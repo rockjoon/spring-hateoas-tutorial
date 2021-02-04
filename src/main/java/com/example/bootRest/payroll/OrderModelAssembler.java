964a10ac7c8a4977dfccd3abd169013c5f0eb42b
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-02-04 23:41:51 +0900

    feat : Order Assembler추가

---
+++ src/main/java/com/example/bootRest/payroll/OrderModelAssembler.java
@@ -0,0 +1,14 @@
+package com.example.bootRest.payroll;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
+
+    @Override
+    public EntityModel<Order> toModel(Order entity) {
+        return null;
+    }
+}
