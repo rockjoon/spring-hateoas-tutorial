b3124951363665e517536b2d536ba97315cb57e2
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-02-04 23:42:18 +0900

    feat : Order GET/POST 매핑 추가

---
+++ src/main/java/com/example/bootRest/payroll/OrderController.java
@@ -0,0 +1,53 @@
+package com.example.bootRest.payroll;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+    private final OrderModelAssembler assembler;
+
+    public OrderController(OrderRepository orderRepository, OrderModelAssembler assembler) {
+        this.orderRepository = orderRepository;
+        this.assembler = assembler;
+    }
+
+    @GetMapping("/orders")
+    CollectionModel<EntityModel<Order>> all() {
+        List<EntityModel<Order>> orders = orderRepository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(orders,
+                linkTo(methodOn(OrderController.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/orders/{id}")
+    EntityModel<Order> one(@PathVariable Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        return assembler.toModel(order);
+    }
+
+
+    @PostMapping("/orders")
+    ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
+        order.setStatus(Status.IN_PROGRESS);
+        Order newORder = orderRepository.save(order);
+
+        return ResponseEntity
+                .created(linkTo(methodOn(OrderController.class).one(order.getId())).toUri())
+                .body(assembler.toModel(newORder));
+    }
+}
